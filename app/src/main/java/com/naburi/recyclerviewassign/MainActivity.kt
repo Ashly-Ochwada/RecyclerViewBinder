d905176c07cd1bb6010845962510c0b466729790
Ashly-Ochwada <ashlywochwada@gmail.com>
2022-07-02 07:48:01 +0300

    recycler view

---
+++ app/src/main/java/com/naburi/recyclerviewassign/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.naburi.recyclerviewassign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.naburi.recyclerviewassign.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var numberList = Addition(100)
+        print(numberList)
+        binding.rvNums.layoutManager = LinearLayoutManager(this)
+        binding.rvNums.adapter= NumberRecyclerViewAdapter(numberList)
+    }
+
+    fun Addition(size:Int):List<Int>{
+        var list = ArrayList<Int>()
+        var t1 = 1
+        var t2 = 0
+        var n = 0
+
+        while(n <= size) {
+            print(t1)
+            var sum = t1 + t2
+            t1 = t2
+            t2 = sum
+            n++
+            list+=sum
+        }
+        return list
+    }
+}
+
+
+
+
